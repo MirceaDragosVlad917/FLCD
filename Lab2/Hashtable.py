748e3803c95ee48ed585585348c77af78cd3c90a
917_Mircea-Dragos_Vlad <dragos.vlad1603@gmail.com>
2022-10-24 00:10:29 +0300

    Added specifications and made find function return the index

--- Lab2/Hashtable.py
+++ Lab2/Hashtable.py
@@ -12,58 +12,73 @@ class Node:
 
 
 class HashTable:
-    """
-    https://github.com/MirceaDragosVlad917/FLCD
-    """
     def __init__(self):
         self.capacity = 47
         self.size = 0
         self.buckets = [None] * self.capacity
 
     def hash(self, key):
-        hashsum = 0
-        for idx, c in enumerate(key):
-            hashsum += (idx + len(key)) ** ord(c)
-            hashsum = hashsum % self.capacity
+        """
+        This function computes the hashcode of a given key
+        :param key: string, the key that is hashed
+        :return: the hash as the absolute value of the remainder of the division of the hash code of the key to the capacity of the table
+        """
+        hashsum = abs(hash(key) % self.capacity)
         return hashsum
 
     def insert(self, key, value):
-        self.size += 1
+        """
+        This function adds an element of the type (key, value) to the hash table
+        :param key: string, the key to be added
+        :param value: string, the value to be added to the corresponding key
+        :return: -
+        """
         index = self.hash(key)
         node = self.buckets[index]
-
         if node is None:
             self.buckets[index] = Node(key, value)
+            self.size += 1
             return
-
         prev = node
         while node is not None:
             prev = node
+            if prev.key == key:
+                return
             node = node.next
-
+        self.size += 1
         prev.next = Node(key, value)
 
     def find(self, key):
+        """
+        This function finds the key in the hash table, if it exists
+        :param key: string, the key that is searched for
+        :return: None, if the key does not exist
+                 The value of the key and it's index in the bucket
+        """
         index = self.hash(key)
         node = self.buckets[index]
-
+        i = 0
         while node is not None and node.key != key:
             node = node.next
-
+            i += 1
         if node is None:
             return None
         else:
-            return node.value
+            return node.value, i
 
     def remove(self, key):
+        """
+        This function deletes a node from the hash table based on the given key
+        :param key: string, the key to be deleted
+        :return: None, if the key does not exist
+                 The value of the node that is deleted
+        """
         index = self.hash(key)
         node = self.buckets[index]
         prev = None
-
         while node is not None and node.key != key:
             prev = node
             node = node.next
-
         if node is None:
             return None
         else:
--- Lab2/Tests.py
+++ Lab2/Tests.py
@@ -12,24 +12,33 @@ class TestHashTable(unittest.TestCase):
 
     def test_insert(self):
         self.assertEqual(self.ht.size, 0)
-        self.ht.insert("test_key", "test_value")
+        self.ht.insert("key", "value")
         self.assertEqual(self.ht.size, 1)
-        self.assertEqual(self.ht.buckets[self.ht.hash("test_key")].value, "test_value")
+        self.ht.insert("key", "value2")
+        self.assertEqual(self.ht.buckets[self.ht.hash("key")].value, "value")
+        self.assertNotEqual(self.ht.size, 2)
 
     def test_find(self):
         self.assertEqual(self.ht.size, 0)
         obj = "find"
         self.ht.insert("key1", obj)
-        self.assertEqual(obj, self.ht.find("key1"))
+        value, position = self.ht.find("key1")
+        self.assertEqual(obj, value)
+        self.assertEqual(0, position)
+        obj = "find2"
+        self.ht.insert("key2", obj)
+        value, position = self.ht.find("key2")
+        self.assertEqual(obj, value)
+        self.assertEqual(0, position)
 
     def test_remove(self):
         self.assertEqual(self.ht.size, 0)
-        obj = "test remove"
+        obj = "remove"
         self.ht.insert("key1", obj)
         self.assertEqual(1, self.ht.size)
         self.assertEqual(obj, self.ht.remove("key1"))
         self.assertEqual(0, self.ht.size)
-        self.assertEqual(None, self.ht.remove("some random key"))
+        self.assertEqual(None, self.ht.remove("key2"))
 
     def test_capacity(self):
         for i in range(0, 1000):
