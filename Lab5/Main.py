43e7728b1ad81f29464f453d5807c943696f7d26
917_Mircea-Dragos_Vlad <dragos.vlad1603@gmail.com>
2022-11-28 13:51:55 +0200

    Work during lab

---
+++ Lab5/Main.py
@@ -0,0 +1,40 @@
+from Model.Grammar import Grammar
+
+
+def menu():
+    print("\n")
+    print("0. Exit")
+    print("1. Print the non-terminals")
+    print("2. Print the terminals")
+    print("3. Print the productions")
+    print("4. Print the productions for a given non-terminal")
+    print("5. Check CFG \n")
+
+def run():
+    grammar = Grammar("Resources/g1.txt")
+    grammar.readGrammar()
+    while 1:
+        menu()
+        try:
+            option = int(input("What is your choice? -> "))
+        except ValueError as ve:
+            option = 100
+        match option:
+            case 0:
+                return
+            case 1:
+                grammar.printNonTerminals()
+            case 2:
+                grammar.printTerminals()
+            case 3:
+                grammar.printProductions()
+            case 4:
+                grammar.productionsForNonTerminal()
+            case 5:
+                print(grammar.checkCFG())
+            case _:
+                print("Invalid choice!")
+
+
+if __name__ == "__main__":
+    run()
--- Lab5/Model/Grammar.py
+++ Lab5/Model/Grammar.py
@@ -1,9 +1,11 @@
 class Grammar:
-    def __init__(self):
-        self.nonTerminals = []
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.nonTerminals = set()
         self.terminals = set()
         self.productions = []
         self.startingSymbol = ""
+        self.readGrammar()
 
     def getNonTerminals(self):
         return self.nonTerminals
@@ -16,6 +18,83 @@ class Grammar:
 
     def getStartingSymbol(self):
         return self.startingSymbol
+
+    def readFromFile(self, line):
+        """
+        This function transforms the line that is read from file into a list of strings
+        :param line: string, the line read from file
+        :return: the line as a list of elements
+        """
+        line = line.split()
+        line = line[-1]
+        line = line[1:-1].split(",")
+        return line
+
+    def readGrammar(self):
+        """
+        This function reads the grammar from a file, line by line, transforming the lines into lists of strings
+        In the file, on the first line we have the non-terminals, on the second one the terminals, on the third one the starting symbol, and from line 4 to the end of the file, the productions
+        :return: -
+        """
+        file = open(self.file_name, 'r')
+        content = file.readlines()
+        index = 0
+        for line in content:
+            if index == 0:
+                self.nonTerminals = self.readFromFile(line)
+            if index == 1:
+                self.terminals = self.readFromFile(line)
+            if index == 2:
+                self.startingSymbol = line.replace(" ", "").strip().split("=")[-1]
+            index = index + 1
+        index = 4
+        while index < len(content):
+            line = content[index].strip().split("->")
+            first = line[0].strip().split(" ")
+            productions = line[1].split("|")
+            second = []
+            for elem in productions:
+                elem = elem.strip().split(" ")
+                second.append(elem)
+            self.productions.append((first, second))
+            index = index + 1
+
+    def productionsForNonTerminal(self):
+        """
+        This function prints the productions for a given non-terminal that is read from keyboard
+        In case the string read from keyboard is not in the list of non-terminals, a message is displayed on the screen
+        :return:
+        """
+        non_terminal = input("Please enter the non-terminal: ")
+        if non_terminal not in self.nonTerminals:
+            print("The non-terminal is not in the list of non terminals!")
+            return
+        for elem in self.productions:
+            if ' '.join(elem[0]) == non_terminal:
+                print("Productions for the given non-terminal: " + str(elem[1]))
+                return
+        print("There are no productions for the given non-terminal!")
+
+    def checkCFG(self):
+        """
+        This function checks if the grammar is context free
+        :return:
+        """
+        for elem in self.productions:
+            if len(elem[0]) > 1:
+                message = "The grammar is not a context free grammar!"
+                return message
+        message = "The grammar is a context free grammar!"
+        return message
+
+    def printNonTerminals(self):
+        print("The non-terminals are: " + str(self.nonTerminals))
+
+    def printTerminals(self):
+        print("The terminals are: " + str(self.terminals))
+
+    def printProductions(self):
+        print("The productions are: " + str(self.productions))
 
     def __str__(self) -> str:
         return "G =( " + str(self.nonTerminals) + ", " + str(self.terminals) + ", " + str(
