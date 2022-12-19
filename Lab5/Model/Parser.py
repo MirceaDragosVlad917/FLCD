a67815d9c782b593eff6c064aedd09c256496282
917_Mircea-Dragos_Vlad <dragos.vlad1603@gmail.com>
2022-12-19 13:48:59 +0200

    Worked on parser table during the lab

--- Lab5/Model/Parser.py
+++ Lab5/Model/Parser.py
@@ -8,12 +8,22 @@ class Parser:
         self.follow = {}
         self.getFirst()
         self.getFollow()
-        # self.parserTable = {}
-        # self.numberedProductions = []
-        # self.number_productions()
-        # self.generate_parser_table()
+        self.parserTable = {}
+        self.numberedProductions = []
+        self.number_productions()
+        self.generate_parser_table()
 
     def FirstAlgorithm(self, non_terminal):
+        """
+        If x is a terminal, then FIRST(x) = { ‘x’ }
+        If x-> Є, is a production rule, then add Є to FIRST(x).
+        If X->Y1 Y2 Y3….Yn is a production,
+            FIRST(X) = FIRST(Y1)
+            If FIRST(Y1) contains Є then FIRST(X) = { FIRST(Y1) – Є } U { FIRST(Y2) }
+            If FIRST (Yi) contains Є for all i = 1 to n, then add Є to FIRST(X).
+        :param non_terminal:
+        :return:
+        """
         if non_terminal in self.grammar.terminals:
             return set(non_terminal)
         first = set()
@@ -42,10 +52,21 @@ class Parser:
             print(elem, ' : ', self.first[elem])
 
     def FollowAlgorithm(self, non_terminal):
+        """
+        1) FOLLOW(S) = { $ }   // where S is the starting Non-Terminal
+        2) If A -> pBq is a production, where p, B and q are any grammar symbols,
+           then everything in FIRST(q)  except Є is in FOLLOW(B).
+        3) If A->pB is a production, then everything in FOLLOW(A) is in FOLLOW(B).
+        4) If A->pBq is a production and FIRST(q) contains Є,
+           then FOLLOW(B) contains { FIRST(q) – Є } U FOLLOW(A)
+        :param non_terminal:
+        :return:
+        """
         follow = set()
         if non_terminal == self.grammar.startingSymbol:
             follow.add('$')
         for production in self.grammar.getProductionsContainingNonTerminal(non_terminal):
+            print(production)
             left = production[0][0]
             right = production[1]
             for i in range(len(right)):
@@ -73,4 +94,13 @@ class Parser:
         for elem in self.follow.keys():
             print(elem, ' : ', self.follow[elem])
 
-    
+    def number_productions(self):
+        index = 1
+        for production in self.grammar.productions:
+            start = production[0]
+            rules = production[1]
+            for rule in rules:
+                self.numberedProductions.append(((start, rule), index))
+                index += 1
+
+
